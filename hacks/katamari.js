c29e3b46558ee71b80b725e5e2a3c4c6555fd5b7
THE Smoreh <108205817+the-Smoreh@users.noreply.github.com>
2025-10-21 08:26:28 -0500

    Create katamari.js

---
+++ hacks/katamari.js
@@ -0,0 +1,1 @@
+javascript:var s = document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='https://cdn.jsdelivr.net/gh/skysthelimitt/Selenite/js/katamari.js';void(0);
