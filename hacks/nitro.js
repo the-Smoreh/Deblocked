b56584503aabe6a262424859264e067515923224
THE Smoreh <108205817+the-Smoreh@users.noreply.github.com>
2025-11-14 09:01:25 -0600

    Update nitro.js

--- hacks/nitro.js
+++ hacks/nitro.js
@@ -1,1 +1,26 @@
-javascript:var win,accuracy=100,t=Object.values(document.querySelector("div.dash-copyContainer"))[1].children._owner.stateNode;win=function(r){t.handleKeyPress("character",new KeyboardEvent("keypress",{key:r?t.props.lessonContent[t.typedIndex]:"$"}))}, m=t.input.keyHandler,t.input.keyHandler=function(e,n){if(n.key==="\n"){t.props.useNitro();return;};"character"===e&&win(Math.random()<(accuracy/100));};
+var win,
+    accuracy = 100,
+    t = Object.values(
+        document.querySelector("div.dash-copyContainer")
+    )[1].children._owner.stateNode;
+
+win = function (r) {
+    t.handleKeyPress(
+        "character",
+        new KeyboardEvent("keypress", {
+            key: r ? t.props.lessonContent[t.typedIndex] : "$"
+        })
+    );
+};
+
+let m = t.input.keyHandler;
+
+t.input.keyHandler = function (e, n) {
+    if (n.key === "\n") {
+        t.props.useNitro();
+        return;
+    }
+    if (e === "character") {
+        win(Math.random() < (accuracy / 100));
+    }
+};
