c11b9ae6a76082dda88627b73d96afd6d8f39307
THE Smoreh <108205817+the-Smoreh@users.noreply.github.com>
2026-01-12 02:23:50 -0600

    Update whistle.js

--- hacks/whistle.js
+++ hacks/whistle.js
@@ -16,15 +16,15 @@ document.body.appendChild(U);
 const cv=U.querySelector("#dwpp_bg"),cx=cv.getContext("2d"),note=U.querySelector("#dwpp_note"),risk=U.querySelector("#dwpp_risk");
 
 const freqs=[
-{hz:12000,txt:"Typical Limit",risk:"🔴 Very High – easily heard",rc:"#ff7070"},
-{hz:14000,txt:"30+ often can't hear",risk:"🟠 High – noticeable to many adults",rc:"#ffaa47"},
-{hz:16000,txt:"25+ lose sensitivity",risk:"🟡 Medium – ALOT of people detect",rc:"#ffd84d"},
-{hz:17000,txt:"Mostly teens/young adults",risk:"🟢 Low – CAN be Heard",rc:"#78ff9c"},
-{hz:18000,txt:"Under 20 hear clearly",risk:"🟢 Very Low – Not Hearable to Many",rc:"#66ffc4"},
-{hz:19000,txt:"Under 18 barely audible",risk:"🟢 Very Low – stealthy",rc:"#66ffc4"},
-{hz:20000,txt:"<17 yrs limit",risk:"🟣 Minimal – near ultrasonic",rc:"#a39dff"},
-{hz:21000,txt:"<15 yrs exceptional hearing",risk:"🟣 Stealth – ultrasonic edge",rc:"#a39dff"},
-{hz:22000,txt:"<13 yrs only",risk:"⚫ Silent – beyond human range",rc:"#9aa0a6"}
+{hz:12000,txt:"1",risk:"🔴 Very High – easily heard",rc:"#ff7070"},
+{hz:14000,txt:"2",risk:"🟠 High – noticeable to many adults",rc:"#ffaa47"},
+{hz:16000,txt:"3",risk:"🟡 Medium – ALOT of people detect",rc:"#ffd84d"},
+{hz:17000,txt:"4",risk:"🟢 Low – CAN be Heard",rc:"#78ff9c"},
+{hz:18000,txt:"5",risk:"🟢 Very Low – Not Hearable to Many",rc:"#66ffc4"},
+{hz:19000,txt:"6",risk:"🟢 Very Low – stealthy",rc:"#66ffc4"},
+{hz:20000,txt:"7",risk:"🟣 Minimal – near ultrasonic",rc:"#a39dff"},
+{hz:21000,txt:"8",risk:"🟣 Stealth – ultrasonic edge",rc:"#a39dff"},
+{hz:22000,txt:"9",risk:"⚫ Silent – beyond human range",rc:"#9aa0a6"}
 ];
 
 const F=U.querySelector("#dwpp_freqs");
